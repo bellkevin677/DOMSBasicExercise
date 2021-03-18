e07c127adbadecdaee03f7096b78ce635def2ce1
bellkevin677 <bellkevin677@gmail.com>
2021-03-18 10:57:02 -0400

    bonus complete

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const images = document.getElementsByTagName(`img`);
 
 const centered = document.getElementsByClassName(`center`);
 
-const ginosEast = document.querySelector(`#ginos`);
+const ginosEast = document.querySelector(`div`);
 
 const pTags = document.querySelectorAll(`p`);
 
@@ -12,10 +12,10 @@ const h1 = document.querySelector(`h1`);
 h1.innerText = `Matt's Favorite Places To Eat In Sweet Home Chicago!`;
 
 const others = document.querySelector(`#others`);
-others.innerHTML(`<h3>Other favorites</h3>`);
+others.innerHTML = `<h3>Other Favorites</h3>`;
 
-const atag = document.querySelector(`a`);
-atag.href = `https://www.choosechicago.com/articles/food-drink/5-classic-chicago-foods/`;
+const aTag = document.querySelector(`a`);
+aTag.href = `https://www.choosechicago.com/articles/food-drink/5-classic-chicago-foods/`;
 
 h1.classList.add(`background`, `text-color`);
 
@@ -28,8 +28,24 @@ body.prepend(h4);
 
 const h5 = document.createElement(`h5`);
 h5.innerText = `See you in the Windy City Sometime!`;
-atag.insertAdjacentElement(`beforeend`, h5);
+aTag.insertAdjacentElement(`afterend`, h5);
 
 const jays = document.querySelector(`li`);
 jays.remove();
+
+const divs = document.querySelectorAll('div');
+
+for (div of divs) {
+    div.classList.toggle(`background`);
+}
+
+const hungry = ["Thanks", "A", "Lot", "Now", "I", "Am", "Hungry"];
+
+for (word of hungry) {
+    // document.querySelectorAll(`body`).append(document.createElement(`span`).innerText = `${word}`);
+    const body1 = document.querySelector(`body`);
+    const span = document.createElement(`span`);
+    span.innerText = `${word}`;
+    body1.append(span);
+}
 
